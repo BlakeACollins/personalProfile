1ed67becc2f2b3a7fb43120e56f6b92537c4356b
Blake Collins <blakeac369@gmail.com>
2021-02-02 21:20:49 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import Navbar from './components/Navbar'
+import Header from './components/Header'
 
 function App() {
   return (
-    <Navbar/>
+    <>
+    <Navbar />
+    <Header />
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='header-wraper'>
+            <div className='main-info'>
+                <h1>Web Design and Development</h1>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Header
