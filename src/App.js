b90cabf3f7ec65805ea821c06cd29da0075c3e00
Blake Collins <blakeac369@gmail.com>
2021-02-03 16:11:35 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import Particles from 'react-particles-js'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
 
 function App() {
   return (
     <>
+    <Particles
+    
+    
+    />
     <Navbar />
     <Header />
     </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Typed from 'react-typed';
 
 const Header = () => {
     return (
         <div className='header-wraper'>
             <div className='main-info'>
                 <h1>Web Design and Development</h1>
+                <Typed className='typed-text'
+                strings={[
+                    "Prototyping","Web Design", "Web Development", "Mobile Web Development", "Mobile Application Development", "SEO Improvements", "Debugging"
+                ]}
+                typeSpeed={40}
+                backSpeed={60}
+                loop >
+                </Typed>
+                <a href="#" className='btn-main-offer'>contact me</a>
             </div>
             
         </div>
