b8bc00ac999e44933a048d5971306930f3297499
Blake Collins <blakeac369@gmail.com>
2021-02-25 20:40:53 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import { BrowserRouter as Router, } from 'react-router-dom';
 import Particles from 'react-particles-js'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
@@ -7,6 +8,7 @@ import Header from './components/Header'
 function App() {
   return (
     <>
+    <Router>
     <Particles
     className='tsparticles-canvas'
       params={{
@@ -31,6 +33,7 @@ function App() {
     />
     <Navbar />
     <Header />
+    </Router>
     </>
   );
 }
