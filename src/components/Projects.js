137d200bdb5606597da09d13abf6354fd106dfda
Blake Collins <blakeac369@gmail.com>
2021-03-09 21:36:01 -0800

    Add files via upload

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,26 +1,29 @@
-import React from 'react';
+
+
 import Amazon from '../img/amazon-project-min.jpg';
 import Netflix from '../img/netflix-project.jpg';
 import GitHub from '../img/github-project.jpg';
 import MealFinder from '../img/meal-finder-project.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearchPlus } from '@fortawesome/free-solid-svg-icons';
-import { Popup, popupboxContainer } from 'reactjs-popup';
+import { PopupboxManager, PopupboxContainer } from 'react-popupbox';
 
 
-function Projects() {
+
+
+const Projects = () => {
     //Amazon Clone
-    const openPopupboxAmazon = () => {
-        
+    const popupboxAmazon = () => {
         const content = (
             <>
-            <img src={Amazon} alt='Amazon Clone project'/>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis convallis convallis tellus id. Dolor sit amet consectetur adipiscing elit.</p>
-            <b>GitHub:</b><a className='hyper__link' onClick={() => window.open("https://github.com/BlakeACollins/amazon-clone")}></a>
+                <img src={Amazon} alt='Amazon Clone project'/>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis convallis convallis tellus id. Dolor sit amet consectetur adipiscing elit.</p>
+                <b>GitHub:</b><a href='https://github.com/BlakeACollins/amazon-clone' className='hyper__link' onClick={() => window.open("https://github.com/BlakeACollins/amazon-clone")}>Amazon Clone</a>
             </>
         )
         
-        Popup.open({ content })
+        PopupboxManager.open({ content });
+           
     }
 
     const popupboxConfigAmazon = {
@@ -38,7 +41,7 @@ function Projects() {
             <div className='container'>
                 <h1 className='text-uppercase text-center py-5' >projects</h1>
                     <div className='image__box__wrapper row justify-content-center'>
-                        <div className='project__image__box' onClick={openPopupboxAmazon}>
+                        <div className='project__image__box' onClick={popupboxAmazon}>
                             <h5 className='text-center mb-3'>Amazon Clone</h5>
                             <img className='project__image' src={Amazon} alt=''/>
                             <div className='overflow'></div>
@@ -67,7 +70,38 @@ function Projects() {
                         </div>
                 </div>
             </div>
-            <popupboxContainer {...popupboxConfigAmazon} />
+            <PopupboxContainer {...popupboxConfigAmazon} />
+            <div className='container mt-3'>
+                    <div className='image__box__wrapper row justify-content-center'>
+                        <div className='project__image__box' onClick={popupboxAmazon}>
+                            <h5 className='text-center mb-3'>Regular Joe Coffee</h5>
+                            <img className='project__image' src={Amazon} alt=''/>
+                            <div className='overflow'></div>
+                            <FontAwesomeIcon className='project__icon' icon={faSearchPlus} />
+                        </div>
+                    {/* - */}
+                        <div className='project__image__box'>
+                            <h5 className='text-center mb-3'>Gmail Clone</h5>
+                            <img className='project__image' src={Netflix} alt=''/>
+                            <div className='overflow'></div>
+                            <FontAwesomeIcon className='project__icon' icon={faSearchPlus} />
+                        </div>
+                    {/* - */}
+                        <div className='project__image__box'>
+                            <h5 className='text-center mb-3'>Web Shop</h5>
+                            <img className='project__image' src={GitHub} alt=''/>
+                            <div className='overflow'></div>
+                            <FontAwesomeIcon className='project__icon' icon={faSearchPlus} />
+                        </div>
+                    {/* - */}
+                        <div className='project__image__box'>
+                            <h5 className='text-center mb-3'>Instagram Clone</h5>
+                            <img className='project__image' src={MealFinder} alt=''/>
+                            <div className='overflow'></div>
+                            <FontAwesomeIcon className='project__icon' icon={faSearchPlus} />
+                        </div>
+                </div>
+            </div>
         </div>
     )
 }
