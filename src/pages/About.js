8e20f0c76b4190dacc88599825cf71008ced4191
Blake Collins <blakeac369@gmail.com>
2021-04-14 21:23:02 -0700

    Add files via upload

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,17 +9,22 @@ const About = () => {
             <div className='row'>
                 <div className='col-lg-6 col-xm-12'>
                     <div className='photo__container'>
-                        <img className='profile__pic mb-5' src={Me} alt='My profile headshot'/>
+                        <img className='profile__pic mb-5' src={Me} alt='Headshot of Blake Collins, the best web developer'/>
                     </div>
                 </div>
 
                 <div className='col-lg-6 col-xm-12'>
-                    <h2 className='about__heading pb-3'>About Me</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut posuere erat. Donec felis ligula, fermentum eu ex ut, maximus dignissim lacus. Donec eleifend mauris nec ultricies fringilla. Proin rhoncus ipsum ut justo vulputate ultricies. Phasellus est eros, tempus a velit eget, tincidunt tempor arcu. Etiam molestie nisi mi, ut vestibulum erat facilisis at. Cras feugiat lectus vel turpis faucibus vestibulum. Integer imperdiet eros et quam commodo, in porttitor nibh interdum. Etiam id tortor metus. Nunc aliquam ultricies odio, dictum lobortis lacus viverra quis. Etiam in sagittis sapien. Curabitur at hendrerit augue. Nullam dictum facilisis erat, id malesuada quam vehicula ut. Etiam purus justo, condimentum sit amet enim sit amet, mollis imperdiet odio.</p>
+                    <h2 className='about__heading pb-3'>Hi, I'm Blake!</h2>
+                   
+                    <p className='about__me'>I am a front-end developer that is fluent in HTML5, CSS3, Bootstrap4, JavaScript, and React. I love web development and all the challenges it can bring. I approach all my projects with a mobile-first design, ensuring that your website will look great on mobile devices, tablets, and desktops! I'm adept at composing websites from scratch or I can utilize popular website makers like WordPress, Wix, Squarespace, or Webflow, to get your site up and running fast!
+                    </p>
+                    <p className='about__me'>I believe your website should be a marketing asset, not an engineering nightmare. Let me worry about your site so you can focus on your business.</p>
                 </div>
             </div>
+            
         </div>
     )
+    
 }
 
 export default About
