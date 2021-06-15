364f46d1ef84ced3b5fd47dfcb9385dad66acb13
Blake Collins <blakeac369@gmail.com>
2021-06-14 20:54:48 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import Menu from './components/Menu'
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
-import Services from './pages/Services'
 import Projects from './pages/Projects'
-import Experience from './pages/Experience'
+
 import Contact from './pages/Contact'
 
 
@@ -18,9 +17,7 @@ function App() {
           <Switch>
             <Route exact path='/' component={Home} />
             <Route exact path='/about' component={About} />
-            <Route exact path='/services' component={Services} />
             <Route exact path='/projects' component={Projects} />
-            <Route exact path='/experience' component={Experience} />
             <Route exact path='/contact' component={Contact} />
           </Switch>
         
